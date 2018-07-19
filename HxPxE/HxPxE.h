a09b6b5e552437798cb4d5b6a91bfe832753d013
Hexagons <anton.heestand@gmail.com>
2018-07-19 21:30:05 +0200

    Initial Commit

---
+++ HxPxE/HxPxE.h
@@ -0,0 +1,19 @@
+//
+//  HxPxE.h
+//  HxPxE
+//
+//  Created by Hexagons on 2018-07-19.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HxPxE.
+FOUNDATION_EXPORT double HxPxEVersionNumber;
+
+//! Project version string for HxPxE.
+FOUNDATION_EXPORT const unsigned char HxPxEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HxPxE/PublicHeader.h>
+
+
