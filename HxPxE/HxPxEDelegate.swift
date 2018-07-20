0a74612a9b18f50170097e158f606d43f4b525b5
Hexagons <anton.heestand@gmail.com>
2018-07-20 22:10:49 +0200

    creates main object class and delegate protocol

---
+++ HxPxE/HxPxEDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  HxPxEDelegate.swift
+//  HxPxE
+//
+//  Created by Hexagons on 2018-07-20.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+import Foundation
+
+public protocol HxPxEDelegate {
+    
+    func hxpxeFrameLoop()
+    
+}
