45ea14818236a500b49537202223808c60994256
Hexagons <anton.heestand@gmail.com>
2018-08-08 13:18:00 +0200

    moves PIXMetalView

---
+++ Source/PIX/View/PIXMetalView.swift
@@ -0,0 +1,63 @@
+//
+//  PIXMetalView.swift
+//  HxPxE
+//
+//  Created by Hexagons on 2018-08-07.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+import MetalKit
+
+public class PIXMetalView: MTKView {
+    
+    var resolution: CGSize?
+    
+    var readyToRender: (() -> ())?
+    
+    public init() {
+        
+        super.init(frame: .zero, device: HxPxE.main.metalDevice)
+        
+        colorPixelFormat = HxPxE.main.bitMode.pixelFormat
+        isOpaque = false
+        framebufferOnly = false
+        autoResizeDrawable = false // CHECK
+        contentMode = .scaleToFill //.scaleAspectFit
+        enableSetNeedsDisplay = true
+        isUserInteractionEnabled = false // CHECK
+        contentScaleFactor = UIScreen.main.scale
+        isPaused = true // CHECK
+        
+    }
+    
+    // MARK: Draw
+    
+    override public func draw(_ rect: CGRect) {
+        autoreleasepool { // CHECK
+            if rect.width > 0 && rect.height > 0 {
+                if resolution != nil {
+                    print("HxPxE PIX is Ready to Render")
+                    readyToRender?()
+                } else {
+                    print("HxPxE ERROR:", "PIX View:", "Draw:", "Resolution not set.")
+                }
+            } else {
+                print("HxPxE ERROR:", "PIX View:", "Draw:", "Rect is zero.")
+            }
+        }
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    func setResolution(_ newResolution: CGSize) {
+        drawableSize = newResolution
+        resolution = newResolution
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
