81d63ed3ac5e14bf997949749b5fd132c2df65d0
Hexagons <anton.heestand@gmail.com>
2018-08-10 21:16:42 +0200

    moves PIX.Kinds to new file

---
+++ Source/PIX/PIXKinds.swift
@@ -0,0 +1,38 @@
+//
+//  PIXDecleration.swift
+//  HxPxE
+//
+//  Created by Hexagons on 2018-08-10.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+import Foundation
+
+extension PIX {
+    
+    enum Kind: String, Codable {
+        case camera
+        case levels
+        case blur
+        case res
+        case edge
+        case image
+        case circle
+        case gradient
+        case lumaBlur
+        var type: PIX.Type {
+            switch self {
+            case .camera: return CameraPIX.self
+            case .levels: return LevelsPIX.self
+            case .blur: return BlurPIX.self
+            case .res: return ResPIX.self
+            case .edge: return EdgePIX.self
+            case .image: return ImagePIX.self
+            case .circle: return CirclePIX.self
+            case .gradient: return GradientPIX.self
+            case .lumaBlur: return LumaBlurPIX.self
+            }
+        }
+    }
+    
+}
