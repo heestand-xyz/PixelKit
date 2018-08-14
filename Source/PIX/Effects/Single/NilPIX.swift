9fe90361cd80db807a83701ee494e8cf22547d07
Hexagons <anton.heestand@gmail.com>
2018-08-15 00:50:15 +0200

    adds NilPIX

---
+++ Source/PIX/Effects/Single/NilPIX.swift
@@ -0,0 +1,15 @@
+//
+//  NilPIX.swift
+//  HxPxE
+//
+//  Created by Hexagons on 2018-08-15.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+import Foundation
+
+public class NilPIX: PIXSingleEffect, PIXable {
+    
+    let kind: PIX.Kind = .nil
+    
+}
