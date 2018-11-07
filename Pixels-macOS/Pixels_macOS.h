f4ca134db74d09275e4cb8c2d9a060657d36c5cc
Hexagons <anton.heestand@gmail.com>
2018-11-07 14:13:15 +0100

    adds macOS target

---
+++ Pixels-macOS/Pixels_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Pixels_macOS.h
+//  Pixels-macOS
+//
+//  Created by Anton Heestand on 2018-11-07.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pixels_macOS.
+FOUNDATION_EXPORT double Pixels_macOSVersionNumber;
+
+//! Project version string for Pixels_macOS.
+FOUNDATION_EXPORT const unsigned char Pixels_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pixels_macOS/PublicHeader.h>
+
+
