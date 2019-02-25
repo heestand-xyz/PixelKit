3ef69b8ffb6dea934f86d71faf08284b01a38476
Hexagons <anton.heestand@gmail.com>
2019-02-25 12:41:52 +0100

    new gradient color step

--- Source/PIX/Content/Generator/GradientPIX.swift
+++ Source/PIX/Content/Generator/GradientPIX.swift
@@ -5,7 +5,17 @@
 //  Created by Hexagons on 2018-08-09.
 //  Open Source - MIT License
 //
-import CoreGraphics//x
+
+import CoreGraphics
+
+public struct ColorStep {
+    public var step: LiveFloat
+    public var color: LiveColor
+    public init(_ step: LiveFloat, _ color: LiveColor) {
+        self.step = step
+        self.color = color
+    }
+}
 
 public class GradientPIX: PIXGenerator {
     
@@ -35,7 +45,7 @@ public class GradientPIX: PIXGenerator {
     public var offset: LiveFloat = 0.0
     public var position: LivePoint = .zero
     public var extendRamp: ExtendMode = .hold { didSet { setNeedsRender() } }
-    public var colorSteps: [(LiveFloat, LiveColor)] = [(0.0, .black), (1.0, .white)]
+    public var colorSteps: [ColorStep] = [ColorStep(0.0, .black), ColorStep(1.0, .white)]
     
     // MARK: - Property Helpers
     
@@ -51,8 +61,8 @@ public class GradientPIX: PIXGenerator {
     }
 
     override var liveArray: [[LiveFloat]] {
-        return colorSteps.map({ fraction, color -> [LiveFloat] in
-            return [fraction, color.r, color.g, color.b, color.a]
+        return colorSteps.map({ colorStep -> [LiveFloat] in
+            return [colorStep.step, colorStep.color.r, colorStep.color.g, colorStep.color.b, colorStep.color.a]
         })
     }
     
@@ -62,12 +72,12 @@ public class GradientPIX: PIXGenerator {
     
     // MARK: - Rainbow
     
-    public static var rainbowColorSteps: [(LiveFloat, LiveColor)] {
-        var colorSteps: [(LiveFloat, LiveColor)] = []
+    public static var rainbowColorSteps: [ColorStep] {
+        var colorSteps: [ColorStep] = []
         let count = 7
         for i in 0..<count {
             let fraction = LiveFloat(i) / LiveFloat(count - 1)
-            colorSteps.append((fraction, LiveColor(h: fraction, s: 1.0, v: 1.0, a: 1.0)))
+            colorSteps.append(ColorStep(fraction, LiveColor(h: fraction, s: 1.0, v: 1.0, a: 1.0)))
         }
         return colorSteps
     }
@@ -87,7 +97,7 @@ public extension PIXOut {
         let res: PIX.Res = Pixels.main.bits == ._8 ? ._256 : ._8192
         let gradientPix = GradientPIX(res: .custom(w: res.w, h: 1))
         gradientPix.name = "gradientMap:gradient"
-        gradientPix.colorSteps = [(0.0, firstColor), (1.0, lastColor)]
+        gradientPix.colorSteps = [ColorStep(0.0, firstColor), ColorStep(1.0, lastColor)]
         lookupPix.inPixB = gradientPix
         return lookupPix
     }
