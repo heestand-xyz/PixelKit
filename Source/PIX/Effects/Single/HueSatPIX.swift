304d140a63daa9fbd14a9b6649981fb2f5f7c77a
Hexagons <anton@hexagons.se>
2019-03-09 14:03:35 +0100

    works on chroma key

--- Source/PIX/Effects/Single/HueSatPIX.swift
+++ Source/PIX/Effects/Single/HueSatPIX.swift
@@ -14,12 +14,12 @@ public class HueSatPIX: PIXSingleEffect {
     // MARK: - Public Properties
 
     public var hue: LiveFloat = 0.0
-    public var saturation: LiveFloat = 1.0
+    public var sat: LiveFloat = 1.0
     
     // MARK: - Property Helpers
     
     override var liveValues: [LiveValue] {
-        return [hue, saturation]
+        return [hue, sat]
     }
     
 }
@@ -27,27 +27,27 @@ public class HueSatPIX: PIXSingleEffect {
 public extension PIXOut {
     
     func _hue(_ hue: LiveFloat) -> HueSatPIX {
-        let hueSaturationPix = HueSatPIX()
-        hueSaturationPix.name = "hue:hueSaturation"
-        hueSaturationPix.inPix = self as? PIX & PIXOut
-        hueSaturationPix.hue = hue
-        return hueSaturationPix
+        let hueSatPix = HueSatPIX()
+        hueSatPix.name = "hue:hueSaturation"
+        hueSatPix.inPix = self as? PIX & PIXOut
+        hueSatPix.hue = hue
+        return hueSatPix
     }
     
-    func _saturation(_ saturation: LiveFloat) -> HueSatPIX {
-        let hueSaturationPix = HueSatPIX()
-        hueSaturationPix.name = "saturation:hueSaturation"
-        hueSaturationPix.inPix = self as? PIX & PIXOut
-        hueSaturationPix.saturation = saturation
-        return hueSaturationPix
+    func _saturation(_ sat: LiveFloat) -> HueSatPIX {
+        let hueSatPix = HueSatPIX()
+        hueSatPix.name = "saturation:hueSaturation"
+        hueSatPix.inPix = self as? PIX & PIXOut
+        hueSatPix.sat = sat
+        return hueSatPix
     }
     
     func _monochrome() -> HueSatPIX {
-        let hueSaturationPix = HueSatPIX()
-        hueSaturationPix.name = "monochrome:hueSaturation"
-        hueSaturationPix.inPix = self as? PIX & PIXOut
-        hueSaturationPix.saturation = 0.0
-        return hueSaturationPix
+        let hueSatPix = HueSatPIX()
+        hueSatPix.name = "monochrome:hueSaturation"
+        hueSatPix.inPix = self as? PIX & PIXOut
+        hueSatPix.sat = 0.0
+        return hueSatPix
     }
     
 }
