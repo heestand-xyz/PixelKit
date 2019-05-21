db7c275e02fd141dd627fc632341cc0a1d66cdaa
Hexagons <anton.heestand@gmail.com>
2019-05-21 09:43:43 +0200

    rm of cached pods

--- Pods/Target Support Files/MetalPerformanceShadersProxy/MetalPerformanceShadersProxy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MetalPerformanceShadersProxy : NSObject
-@end
-@implementation PodsDummy_MetalPerformanceShadersProxy
-@end
--- Pods/Target Support Files/Pods-PixelKit-macOS/Pods-PixelKit-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PixelKit_macOS : NSObject
-@end
-@implementation PodsDummy_Pods_PixelKit_macOS
-@end
