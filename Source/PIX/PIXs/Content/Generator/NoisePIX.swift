1f04bd2f25416e4f581cefe917f797d4cd44ac20
Hexagons <anton@hexagons.se>
2019-06-25 21:31:29 +0200

    noise

--- Source/PIX/PIXs/Content/Generator/NoisePIX.swift
+++ Source/PIX/PIXs/Content/Generator/NoisePIX.swift
@@ -12,7 +12,7 @@ public class NoisePIX: PIXGenerator, PIXAuto {
     
     // MARK: - Public Properties
     
-    public var seed: LiveInt = 0
+    public var seed: LiveInt = 1
     public var octaves: LiveInt = 10
     public var position: LivePoint = .zero
     public var zPosition: LiveFloat = 0.0
