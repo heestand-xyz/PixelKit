5c2c21d9411a026e8376d2008ecc5009a312882c
Hexagons <anton.heestand@gmail.com>
2019-10-16 15:39:06 +0200

    makes single effect tests

--- Tests/Maker/ViewController.swift
+++ Tests/Maker/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import Cocoa
+import RenderKit
 import PixelKit_macOS
 
 class ViewController: NSViewController {
     
     let outputPath = "Code/Frameworks/Production/PixelKit/Resources/Tests/Renders"
-
+    var rendersUrl: URL!
+    var testPix: (PIX & NODEOut)!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,15 +27,31 @@ class ViewController: NSViewController {
         PixelKit.main.render.engine.renderMode = .manual
 
         let documentsUrl = URL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!)
-        let rendersUrl = documentsUrl.appendingPathComponent(outputPath)
+        rendersUrl = documentsUrl.appendingPathComponent(outputPath)
         var isDir: ObjCBool = true
         guard FileManager.default.fileExists(atPath: rendersUrl.path, isDirectory: &isDir) else { fatalError() }
         
-        print(">>>>>> generators")
-        let generatorsUrl = rendersUrl.appendingPathComponent("generators")
-        if !FileManager.default.fileExists(atPath: generatorsUrl.path, isDirectory: &isDir) {
-            try! FileManager.default.createDirectory(at: generatorsUrl, withIntermediateDirectories: false, attributes: nil)
+        testPix = makeTestPix()
+        
+        self.makeGenerators {
+            self.makeSingleEffects {
+                print(">>>>>>>>> test maker done")
+            }
         }
+        
+    }
+    
+    func makeTestPix() -> PIX & NODEOut {
+        let polygonPix = PolygonPIX(at: ._128)
+        let noisePix = NoisePIX(at: ._128)
+        noisePix.colored = true
+        return polygonPix + noisePix
+    }
+    
+    func makeGenerators(done: @escaping () -> ()) {
+        print(">>>>>> generators started")
+        let generatorsUrl = rendersUrl.appendingPathComponent("generators")
+        makeDirectory(at: generatorsUrl)
         var autos = AutoPIXGenerator.allCases
         func render(auto: AutoPIXGenerator) {
             print(">>> generator \(auto.name)")
@@ -45,15 +64,46 @@ class ViewController: NSViewController {
                 pix.destroy()
                 if !autos.isEmpty {
                     render(auto: autos.remove(at: 0))
+                } else {
+                    print(">>>>>> generators done")
+                    done()
                 }
             }
         }
         render(auto: autos.remove(at: 0))
-
-        func done() {
-            print(">>>>>>>>> test maker done")
+    }
+    
+    func makeSingleEffects(done: @escaping () -> ()) {
+        print(">>>>>> single effects started")
+        let singleEffectsUrl = rendersUrl.appendingPathComponent("singleEffects")
+        makeDirectory(at: singleEffectsUrl)
+        var autos = AutoPIXSingleEffect.allCases
+        func render(auto: AutoPIXSingleEffect) {
+            print(">>> single effect \(auto.name)")
+            let pix = auto.pixType.init()
+            pix.input = testPix
+            try! PixelKit.main.render.engine.manuallyRender {
+                guard let image: NSImage = pix.renderedImage else { fatalError() }
+                let imageUrl = singleEffectsUrl.appendingPathComponent("\(auto.name).png")
+                guard image.savePNG(to: imageUrl) else { fatalError() }
+                print(">>> saved \(auto.name)")
+                pix.destroy()
+                if !autos.isEmpty {
+                    render(auto: autos.remove(at: 0))
+                } else {
+                    print(">>>>>> single effects done")
+                    done()
+                }
+            }
         }
-        
+        render(auto: autos.remove(at: 0))
+    }
+    
+    func makeDirectory(at url: URL) {
+        var isDir: ObjCBool = true
+        if !FileManager.default.fileExists(atPath: url.path, isDirectory: &isDir) {
+            try! FileManager.default.createDirectory(at: url, withIntermediateDirectories: false, attributes: nil)
+        }
     }
 
 }
