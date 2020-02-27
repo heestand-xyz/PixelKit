8eae19136160edd2f579d782fffd9781e123ebec
Hexagons <anton@hexagons.se>
2020-02-27 22:19:34 +0100

    pixtools

--- Source/PIX/Auto/PIXAuto.swift
+++ Source/PIX/Auto/PIXAuto.swift
@@ -18,143 +18,123 @@ import RenderKit
 protocol PIXAutoParent {}
 protocol PIXAuto {}
 
-public protocol AutoProperty {
-    var name: String { get }
-}
-
-public class AutoLiveFloatProperty: AutoProperty {
+public class AutoProperty<T> {
     public let name: String
-    public var value: LiveFloat {
+    public var value: T {
         get { return getCallback() }
         set { setCallback(newValue) }
     }
-    let getCallback: () -> (LiveFloat)
-    let setCallback: (LiveFloat) -> ()
-    init(name: String, getCallback: @escaping () -> (LiveFloat), setCallback: @escaping (LiveFloat) -> ()) {
+    let getCallback: () -> (T)
+    let setCallback: (T) -> ()
+    init(name: String, getCallback: @escaping () -> (T), setCallback: @escaping (T) -> ()) {
         self.name = name
         self.getCallback = getCallback
         self.setCallback = setCallback
     }
 }
 
-public class AutoLiveIntProperty: AutoProperty {
-    public let name: String
-    public var value: LiveInt {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (LiveInt)
-    let setCallback: (LiveInt) -> ()
-    init(name: String, getCallback: @escaping () -> (LiveInt), setCallback: @escaping (LiveInt) -> ()) {
-        self.name = name
-        self.getCallback = getCallback
-        self.setCallback = setCallback
-    }
-}
-
-public class AutoLiveBoolProperty: AutoProperty {
-    public let name: String
-    public var value: LiveBool {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (LiveBool)
-    let setCallback: (LiveBool) -> ()
-    init(name: String, getCallback: @escaping () -> (LiveBool), setCallback: @escaping (LiveBool) -> ()) {
-        self.name = name
-        self.getCallback = getCallback
-        self.setCallback = setCallback
-    }
-}
-
-public class AutoLiveColorProperty: AutoProperty {
-    public let name: String
-    public var value: LiveColor {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (LiveColor)
-    let setCallback: (LiveColor) -> ()
-    init(name: String, getCallback: @escaping () -> (LiveColor), setCallback: @escaping (LiveColor) -> ()) {
-        self.name = name
-        self.getCallback = getCallback
-        self.setCallback = setCallback
-    }
-}
-
-public class AutoLivePointProperty: AutoProperty {
-    public let name: String
-    public var value: LivePoint {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (LivePoint)
-    let setCallback: (LivePoint) -> ()
-    init(name: String, getCallback: @escaping () -> (LivePoint), setCallback: @escaping (LivePoint) -> ()) {
-        self.name = name
-        self.getCallback = getCallback
-        self.setCallback = setCallback
-    }
-}
-
-public class AutoLiveSizeProperty: AutoProperty {
-    public let name: String
-    public var value: LiveSize {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (LiveSize)
-    let setCallback: (LiveSize) -> ()
-    init(name: String, getCallback: @escaping () -> (LiveSize), setCallback: @escaping (LiveSize) -> ()) {
-        self.name = name
-        self.getCallback = getCallback
-        self.setCallback = setCallback
-    }
-}
-
-public class AutoLiveRectProperty: AutoProperty {
-    public let name: String
-    public var value: LiveRect {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (LiveRect)
-    let setCallback: (LiveRect) -> ()
-    init(name: String, getCallback: @escaping () -> (LiveRect), setCallback: @escaping (LiveRect) -> ()) {
-        self.name = name
-        self.getCallback = getCallback
-        self.setCallback = setCallback
-    }
-}
-
-public class AutoEnumProperty: AutoProperty {
-    public let name: String
+public class AutoLiveFloatProperty: AutoProperty<LiveFloat> {}
+public class AutoLiveIntProperty: AutoProperty<LiveInt> {}
+public class AutoLiveBoolProperty: AutoProperty<LiveBool> {}
+public class AutoLiveColorProperty: AutoProperty<LiveColor> {}
+public class AutoLivePointProperty: AutoProperty<LivePoint> {}
+public class AutoLiveSizeProperty: AutoProperty<LiveSize> {}
+public class AutoLiveRectProperty: AutoProperty<LiveRect> {}
+public class AutoEnumProperty: AutoProperty<String> {
     public let cases: [String]
-    public var value: String {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (String)
-    let setCallback: (String) -> ()
     init(name: String, cases: [String], getCallback: @escaping () -> (String), setCallback: @escaping (String) -> ()) {
-        self.name = name
         self.cases = cases
-        self.getCallback = getCallback
-        self.setCallback = setCallback
+        super.init(name: name, getCallback: getCallback, setCallback: setCallback)
     }
 }
+public class AutoLiveVecProperty: AutoProperty<LiveVec> {}
 
-public class AutoLiveVecProperty: AutoProperty {
-    public let name: String
-    public var value: LiveVec {
-        get { return getCallback() }
-        set { setCallback(newValue) }
-    }
-    let getCallback: () -> (LiveVec)
-    let setCallback: (LiveVec) -> ()
-    init(name: String, getCallback: @escaping () -> (LiveVec), setCallback: @escaping (LiveVec) -> ()) {
-        self.name = name
-        self.getCallback = getCallback
-        self.setCallback = setCallback
+public extension AutoPIXSingleEffect {
+    func allAutoLivePropertiesAsFloats(for pix: PIXSingleEffect) -> [AutoLiveFloatProperty] {
+        var all: [AutoLiveFloatProperty] = []
+        all.append(contentsOf: autoLiveFloats(for: pix))
+        all.append(contentsOf: autoLiveInts(for: pix).map { auto -> AutoLiveFloatProperty in
+            AutoLiveFloatProperty(name: auto.name, getCallback: {
+                LiveFloat(auto.value)
+            }) { value in
+                auto.value = LiveInt(value)
+            }
+        })
+        all.append(contentsOf: autoLiveBools(for: pix).map { auto -> AutoLiveFloatProperty in
+            AutoLiveFloatProperty(name: auto.name, getCallback: {
+                auto.value <?> 1.0 <=> 0.0
+            }) { value in
+                auto.value = value > 0.0
+            }
+        })
+        for auto in autoLiveColors(for: pix) {
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-red", getCallback: { () -> (LiveFloat) in
+                auto.value.r
+            }, setCallback: { value in
+                auto.value.r = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-green", getCallback: { () -> (LiveFloat) in
+                auto.value.r
+            }, setCallback: { value in
+                auto.value.r = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-blue", getCallback: { () -> (LiveFloat) in
+                auto.value.r
+            }, setCallback: { value in
+                auto.value.r = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-alpha", getCallback: { () -> (LiveFloat) in
+                auto.value.a
+            }, setCallback: { value in
+                auto.value.a = value
+            }))
+        }
+        for auto in autoLivePoints(for: pix) {
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-x", getCallback: { () -> (LiveFloat) in
+                auto.value.x
+            }, setCallback: { value in
+                auto.value.x = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-y", getCallback: { () -> (LiveFloat) in
+                auto.value.y
+            }, setCallback: { value in
+                auto.value.y = value
+            }))
+        }
+        for auto in autoLiveSizes(for: pix) {
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-w", getCallback: { () -> (LiveFloat) in
+                auto.value.w
+            }, setCallback: { value in
+                auto.value.w = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-h", getCallback: { () -> (LiveFloat) in
+                auto.value.h
+            }, setCallback: { value in
+                auto.value.h = value
+            }))
+        }
+        for auto in autoLiveRects(for: pix) {
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-x", getCallback: { () -> (LiveFloat) in
+                auto.value.x
+            }, setCallback: { value in
+                auto.value.x = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-y", getCallback: { () -> (LiveFloat) in
+                auto.value.y
+            }, setCallback: { value in
+                auto.value.y = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-w", getCallback: { () -> (LiveFloat) in
+                auto.value.w
+            }, setCallback: { value in
+                auto.value.w = value
+            }))
+            all.append(AutoLiveFloatProperty(name: "\(auto.name)-h", getCallback: { () -> (LiveFloat) in
+                auto.value.h
+            }, setCallback: { value in
+                auto.value.h = value
+            }))
+        }
+        return all
     }
 }
