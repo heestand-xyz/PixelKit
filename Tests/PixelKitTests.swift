54e36237072bfe6d3935c77ce11c4b4541d0d1c4
Hexagons <anton@hexagons.se>
2020-04-10 16:25:52 +0200

    testAveragePixGenerators

--- Tests/PixelKitTests.swift
+++ Tests/PixelKitTests.swift
@@ -40,48 +40,6 @@ class PixelKitTests: XCTestCase {
 
     override func tearDown() {}
 
-    func testAveragePixGenerators() {
-        
-        setuUpManual()
-        
-        /// 8bit at 128x128
-        let averages: [AutoPIXGenerator: CGFloat] = [
-            .arcpix: 0.047119140625,
-            .circlepix: 0.197021484375,
-            .colorpix: 1.0,
-            .gradientpix: 0.5,
-            .linepix: 0.0115966796875,
-            .noisepix: 0.4070302925856992,
-            .polygonpix: 0.16357421875,
-            .rectanglepix: 0.19140625
-        ]
-        
-        for average in averages {
-            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-            
-            print("testing", average.key.name)
-            let pix = average.key.pixType.init(at: ._128)
-                    
-            let expect = XCTestExpectation()
-            try! PixelKit.main.render.engine.manuallyRender {
-                guard let pixels = pix.renderedPixels else {
-                    XCTAssert(false);
-                    expect.fulfill();
-                    return
-                }
-                let lum = pixels.average.lum.cg
-                print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", lum)
-                XCTAssert(lum == average.value, "\(average.key.name) average should be \(average.value) and was \(lum)")
-                expect.fulfill()
-            }
-            self.wait(for: [expect], timeout: 1.0)
-            
-            pix.destroy()
-            
-        }
-        
-    }
-
     func testShapePixGenerators() {
         
         setuUpManual()
--- Tests/PixelKitTests/PixelKitTests.swift
+++ Tests/PixelKitTests/PixelKitTests.swift
@@ -46,7 +46,10 @@ final class PixelKitTests: XCTestCase {
         PixelKit.main.render.logger.logAll()
         PixelKit.main.render.engine.logger.logAll()
         
-//        PixelKit.main.render.engine.renderMode = .manual
+    }
+    
+    func setUpManual() {
+        PixelKit.main.render.engine.renderMode = .manual
     }
     
     func testMetalPixs() {
@@ -58,9 +61,49 @@ final class PixelKitTests: XCTestCase {
             XCTAssertNotNil(pix.name)
         }
     }
-
+    
+//    func testAveragePixGenerators() {
+//
+//        setUpManual()
+//
+//        /// 8bit at 128x128
+//        let averages: [AutoPIXGenerator: CGFloat] = [
+//            .arcpix: 0.047119140625,
+//            .circlepix: 0.197021484375,
+//            .colorpix: 1.0,
+//            .gradientpix: 0.5,
+//            .linepix: 0.0115966796875,
+//            .noisepix: 0.4070302925856992,
+//            .polygonpix: 0.16357421875,
+//            .rectanglepix: 0.19140625
+//        ]
+//
+//        for average in averages {
+//            print("testing", average.key.name)
+//            let pix = average.key.pixType.init(at: ._128)
+//
+//            let expect = XCTestExpectation()
+//            try! PixelKit.main.render.engine.manuallyRender {
+//                guard let pixels = pix.renderedPixels else {
+//                    XCTAssert(false);
+//                    expect.fulfill();
+//                    return
+//                }
+//                let lum = pixels.average.lum.cg
+//                XCTAssert(lum == average.value, "\(average.key.name) average should be \(average.value) and was \(lum)")
+//                expect.fulfill()
+//            }
+//            self.wait(for: [expect], timeout: 1.0)
+//
+//            pix.destroy()
+//
+//        }
+//
+//    }
+    
     static var allTests = [
         ("testMetalPixs", testMetalPixs),
+//        ("testAveragePixGenerators", testAveragePixGenerators),
     ]
     
 }
