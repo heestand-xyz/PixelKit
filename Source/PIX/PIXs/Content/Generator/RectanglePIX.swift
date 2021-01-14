68468e0758348958a300f37aa129754aa6e74005
Anton Heestand <anton.heestand@gmail.com>
2021-01-14 19:25:59 +0100

    Rectangle

--- Source/PIX/PIXs/Content/Generator/RectanglePIX.swift
+++ Source/PIX/PIXs/Content/Generator/RectanglePIX.swift
@@ -15,14 +15,18 @@ public class RectanglePIX: PIXGenerator {
     
     // MARK: - Public Properties
     
-    public var position: CGPoint = .zero
-    public var size: CGSize = CGSize(width: 0.5, height: 0.5)
-    public var cornerRadius: CGFloat = 0.0
+    @Live public var position: CGPoint = .zero
+    @Live public var size: CGSize = CGSize(width: 0.5, height: 0.5)
+    @Live public var cornerRadius: CGFloat = 0.0
     
     // MARK: - Property Helpers
     
+    public override var liveList: [LiveProp] {
+        super.liveList + [_position, _size, _cornerRadius]
+    }
+    
     override public var values: [Floatable] {
-        return [size, position/*, rotation*/, cornerRadius, super.color, super.backgroundColor]
+        [size, position, cornerRadius, super.color, super.backgroundColor]
     }
     
     public required init(at resolution: Resolution = .auto(render: PixelKit.main.render)) {
