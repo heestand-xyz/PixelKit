5f35241e2fd5f34031b113b0dcebe37d9150f304
Anton Heestand <anton.heestand@bontouch.com>
2021-10-11 18:17:59 +0200

    Time

---
+++ Source/PIX/PIXs/Effects/Single/Add/AddPIX.metal
@@ -0,0 +1,53 @@
+//
+//  EffectSingleAveragePIX.metal
+//  VoxelKitShaders
+//
+//  Created by Anton Heestand on 2019-10-02.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct VertexOut {
+    float4 position [[position]];
+    float2 texCoord;
+};
+
+struct Uniforms {
+    float axis;
+};
+
+fragment float4 addPix(VertexOut out [[stage_in]],
+                       const device Uniforms& in [[ buffer(0) ]],
+                       texture3d<float>  inTex [[ texture(0) ]],
+                       sampler s [[ sampler(0) ]]) {
+    
+    float u = out.texCoord[0];
+    float v = out.texCoord[1];
+    
+    int res = 0;
+    switch (int(in.axis)) {
+        case 0: res = inTex.get_width(); break;
+        case 1: res = inTex.get_height(); break;
+        case 2: res = inTex.get_depth(); break;
+    }
+    
+    float4 c = 0.0;
+    for (int i = 0; i < res; ++i) {
+        
+        float fraction = (float(i) + 0.5) / float(res);
+       
+        float3 crd = 0.0;
+        switch (int(in.axis)) {
+            case 0: crd = float3(fraction, u, v); break;
+            case 1: crd = float3(u, fraction, v); break;
+            case 2: crd = float3(u, v, fraction); break;
+        }
+        
+        c += inTex.sample(s, crd);
+        
+    }
+    
+    return c;
+}
+
---
+++ Source/PIX/PIXs/Effects/Single/Add/AddPIX.swift
@@ -0,0 +1,60 @@
+//
+//  AddPIX.swift
+//  PixelKit
+//
+//  Created by Anton Heestand on 2019-10-05.
+//
+
+import Foundation
+import CoreGraphics
+import RenderKit
+import Resolution
+
+/// Useful with **VoxelKit** to downsample depth images.
+final public class AddPIX: PIXSingleEffect, PIXViewable {
+
+    override public var shaderName: String { return "addPix" }
+
+    // MARK: - Public Properties
+
+    public enum Axis: String, Enumable {
+        case x
+        case y
+        case z
+        public var index: Int {
+            switch self {
+            case .x: return 0
+            case .y: return 1
+            case .z: return 2
+            }
+        }
+        public var typeName: String { rawValue }
+        public var name: String {
+            switch self {
+            case .x: return "X"
+            case .y: return "Y"
+            case .z: return "Z"
+            }
+        }
+    }
+    @LiveEnum("axis") public var axis: Axis = .z
+
+    // MARK: - Property Helpers
+
+    public override var liveList: [LiveWrap] {
+        [_axis]
+    }
+
+    public override var values: [Floatable] {
+        [axis]
+    }
+
+    public required init() {
+        super.init(name: "Add", typeName: "pix-effect-single-add")
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        try super.init(from: decoder)
+    }
+
+}
