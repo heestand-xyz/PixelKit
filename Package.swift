f793dd2c0c40d428601b225be2386dc525b9ae67
Anton Heestand <anton.heestand@bontouch.com>
2021-10-18 10:59:34 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "PixelKit", targets: ["PixelKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/heestand-xyz/RenderKit", from: "1.0.4"),
+        .package(url: "https://github.com/heestand-xyz/RenderKit", from: "1.1.0"),
     ],
     targets: [
         .target(name: "PixelKit", dependencies: ["RenderKit"], path: "Source", exclude: [
