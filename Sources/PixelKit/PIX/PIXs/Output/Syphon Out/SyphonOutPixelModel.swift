79ba3929efb88ccd42408879b9c31ced1c517b44
Anton Heestand <anton.heestand@gmail.com>
2022-01-14 13:28:54 +0100

    Nodes

--- Sources/PixelKit/PIX/PIXs/Output/Syphon Out/SyphonOutPixelModel.swift
+++ Sources/PixelKit/PIX/PIXs/Output/Syphon Out/SyphonOutPixelModel.swift
@@ -2,36 +2,36 @@
 //  Created by Anton Heestand on 2022-01-09.
 //
 
-#if os(macOS)
-
-import Foundation
-import CoreGraphics
-import RenderKit
-import Resolution
-import PixelColor
-
-public struct SyphonOutPixelModel: PixelOutputModel {
-    
-    // MARK: Global
-    
-    public var id: UUID = UUID()
-    public var name: String = "Syphon Out"
-    public var typeName: String = "pix-output-syphon-out"
-    public var bypass: Bool = false
-    
-    public var outputNodeReferences: [NodeReference] = []
-
-    public var viewInterpolation: ViewInterpolation = .linear
-    public var interpolation: PixelInterpolation = .linear
-    public var extend: ExtendMode = .zero
-}
-
-extension SyphonOutPixelModel {
-    
-    public init(from decoder: Decoder) throws {
-        
-        self = try PixelOutputModelDecoder.decode(from: decoder, model: self) as! Self
-    }
-}
-
-#endif
+//#if os(macOS)
+//
+//import Foundation
+//import CoreGraphics
+//import RenderKit
+//import Resolution
+//import PixelColor
+//
+//public struct SyphonOutPixelModel: PixelOutputModel {
+//    
+//    // MARK: Global
+//    
+//    public var id: UUID = UUID()
+//    public var name: String = "Syphon Out"
+//    public var typeName: String = "pix-output-syphon-out"
+//    public var bypass: Bool = false
+//    
+//    public var outputNodeReferences: [NodeReference] = []
+//
+//    public var viewInterpolation: ViewInterpolation = .linear
+//    public var interpolation: PixelInterpolation = .linear
+//    public var extend: ExtendMode = .zero
+//}
+//
+//extension SyphonOutPixelModel {
+//    
+//    public init(from decoder: Decoder) throws {
+//        
+//        self = try PixelOutputModelDecoder.decode(from: decoder, model: self) as! Self
+//    }
+//}
+//
+//#endif
