c49613b08819f41533ff8c4c4b7eb1155bbb6508
Anton Heestand <anton.heestand@gmail.com>
2022-02-20 11:26:50 +0100

    Package Version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "PixelKit", targets: ["PixelKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/heestand-xyz/RenderKit", from: "2.0.0"),
+        .package(url: "https://github.com/heestand-xyz/RenderKit", from: "2.0.1"),
         .package(url: "https://github.com/heestand-xyz/TextureMap", from: "0.1.1"),
         .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.2.1"),
     ],
